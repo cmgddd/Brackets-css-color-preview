5d21f888b4f61563522f301328819f02707c7cda
neil_chen <cmgddd@163.com>
2014-11-13 00:13:39 +0800

    imporve the decisions for css color and something more

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ define(function (require, exports, module) {
         _ = brackets.getModule("thirdparty/lodash"),
         CommandManager = brackets.getModule("command/CommandManager"),
         EditorManager   = brackets.getModule('editor/EditorManager'),
+        DocumentManager   = brackets.getModule('document/DocumentManager'),
         ExtensionUtils          = brackets.getModule("utils/ExtensionUtils"),
         ColorUtils   = brackets.getModule('utils/ColorUtils'),
         Menus = brackets.getModule("command/Menus"),
@@ -45,6 +46,8 @@ define(function (require, exports, module) {
         _prefs = PreferencesManager.getExtensionPrefs("brackets-css-color-preview"),
         gutterName = "CodeMirror-colorGutter",
         _enabled = true;
+
+        COLOR_REGEX = /#[a-f0-9]{6}\b|#[a-f0-9]{3}\b|\brgb\(\s*(?:[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\b\s*,\s*(?:[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\b\s*,\s*(?:[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\b\s*\)|\brgb\(\s*(?:[0-9]{1,2}%|100%)\s*,\s*(?:[0-9]{1,2}%|100%)\s*,\s*(?:[0-9]{1,2}%|100%)\s*\)|\brgba\(\s*(?:[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\b\s*,\s*(?:[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\b\s*,\s*(?:[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\b\s*,\s*(?:1|1\.0|0|0?\.[0-9]{1,3})\s*\)|\brgba\(\s*(?:[0-9]{1,2}%|100%)\s*,\s*(?:[0-9]{1,2}%|100%)\s*,\s*(?:[0-9]{1,2}%|100%)\s*,\s*(?:1|1\.0|0|0?\.[0-9]{1,3})\s*\)|\bhsl\(\s*(?:[0-9]{1,3})\b\s*,\s*(?:[0-9]{1,2}|100)\b%\s*,\s*(?:[0-9]{1,2}|100)\b%\s*\)|\bhsla\(\s*(?:[0-9]{1,3})\b\s*,\s*(?:[0-9]{1,2}|100)\b%\s*,\s*(?:[0-9]{1,2}|100)\b%\s*,\s*(?:1|1\.0|0|0?\.[0-9]{1,3})\s*\)|[^.-]aliceblue[^-]|[^.-]antiquewhite[^-]|[^.-]aqua[^-]|[^.-]aquamarine[^-]|[^.-]azure[^-]|[^.-]beige[^-]|[^.-]bisque[^-]|[^.-]black[^-]|[^.-]blanchedalmond[^-]|[^.-]blue[^-]|[^.-]blueviolet[^-]|[^.-]brown[^-]|[^.-]burlywood[^-]|[^.-]cadetblue[^-]|[^.-]chartreuse[^-]|[^.-]chocolate[^-]|[^.-]coral[^-]|[^.-]cornflowerblue[^-]|[^.-]cornsilk[^-]|[^.-]crimson[^-]|[^.-]cyan[^-]|[^.-]darkblue[^-]|[^.-]darkcyan[^-]|[^.-]darkgoldenrod[^-]|[^.-]darkgray[^-]|[^.-]darkgreen[^-]|[^.-]darkgrey[^-]|[^.-]darkkhaki[^-]|[^.-]darkmagenta[^-]|[^.-]darkolivegreen[^-]|[^.-]darkorange[^-]|[^.-]darkorchid[^-]|[^.-]darkred[^-]|[^.-]darksalmon[^-]|[^.-]darkseagreen[^-]|[^.-]darkslateblue[^-]|[^.-]darkslategray[^-]|[^.-]darkslategrey[^-]|[^.-]darkturquoise[^-]|[^.-]darkviolet[^-]|[^.-]deeppink[^-]|[^.-]deepskyblue[^-]|[^.-]dimgray[^-]|[^.-]dimgrey[^-]|[^.-]dodgerblue[^-]|[^.-]firebrick[^-]|[^.-]floralwhite[^-]|[^.-]forestgreen[^-]|[^.-]fuchsia[^-]|[^.-]gainsboro[^-]|[^.-]ghostwhite[^-]|[^.-]gold[^-]|[^.-]goldenrod[^-]|[^.-]gray[^-]|[^.-]green[^-]|[^.-]greenyellow[^-]|[^.-]grey[^-]|[^.-]honeydew[^-]|[^.-]hotpink[^-]|[^.-]indianred[^-]|[^.-]indigo[^-]|[^.-]ivory[^-]|[^.-]khaki[^-]|[^.-]lavender[^-]|[^.-]lavenderblush[^-]|[^.-]lawngreen[^-]|[^.-]lemonchiffon[^-]|[^.-]lightblue[^-]|[^.-]lightcoral[^-]|[^.-]lightcyan[^-]|[^.-]lightgoldenrodyellow[^-]|[^.-]lightgray[^-]|[^.-]lightgreen[^-]|[^.-]lightgrey[^-]|[^.-]lightpink[^-]|[^.-]lightsalmon[^-]|[^.-]lightseagreen[^-]|[^.-]lightskyblue[^-]|[^.-]lightslategray[^-]|[^.-]lightslategrey[^-]|[^.-]lightsteelblue[^-]|[^.-]lightyellow[^-]|[^.-]lime[^-]|[^.-]limegreen[^-]|[^.-]linen[^-]|[^.-]magenta[^-]|[^.-]maroon[^-]|[^.-]mediumaquamarine[^-]|[^.-]mediumblue[^-]|[^.-]mediumorchid[^-]|[^.-]mediumpurple[^-]|[^.-]mediumseagreen[^-]|[^.-]mediumslateblue[^-]|[^.-]mediumspringgreen[^-]|[^.-]mediumturquoise[^-]|[^.-]mediumvioletred[^-]|[^.-]midnightblue[^-]|[^.-]mintcream[^-]|[^.-]mistyrose[^-]|[^.-]moccasin[^-]|[^.-]navajowhite[^-]|[^.-]navy[^-]|[^.-]oldlace[^-]|[^.-]olive[^-]|[^.-]olivedrab[^-]|[^.-]orange[^-]|[^.-]orangered[^-]|[^.-]orchid[^-]|[^.-]palegoldenrod[^-]|[^.-]palegreen[^-]|[^.-]paleturquoise[^-]|[^.-]palevioletred[^-]|[^.-]papayawhip[^-]|[^.-]peachpuff[^-]|[^.-]peru[^-]|[^.-]pink[^-]|[^.-]plum[^-]|[^.-]powderblue[^-]|[^.-]purple[^-]|[^\-.]red[^-]|[^.-]rosybrown[^-]|[^.-]royalblue[^-]|[^.-]saddlebrown[^-]|[^.-]salmon[^-]|[^.-]sandybrown[^-]|[^.-]seagreen[^-]|[^.-]seashell[^-]|[^.-]sienna[^-]|[^.-]silver[^-]|[^.-]skyblue[^-]|[^.-]slateblue[^-]|[^.-]slategray[^-]|[^.-]slategrey[^-]|[^.-]snow[^-]|[^.-]springgreen[^-]|[^.-]steelblue[^-]|[^.-]tan[^-]|[^.-]teal[^-]|[^.-]thistle[^-]|[^.-]tomato[^-]|[^.-]turquoise[^-]|[^.-]violet[^-]|[^.-]wheat[^-]|[^.-]white[^-]|[^.-]whitesmoke[^-]|[^.-]yellow[^-]|[^.-]yellowgreen[^-]/gi;
 
     ExtensionUtils.loadStyleSheet(module, "main.css");
 
@@ -62,7 +65,7 @@ define(function (require, exports, module) {
 
         // Get editor
         getEditor: function() {
-            return EditorManager.getCurrentFullEditor();
+            return EditorManager.getActiveEditor();
         },
         // show color preview
         showColorMarks: function() {
@@ -71,22 +74,26 @@ define(function (require, exports, module) {
             // -- but this is considered deprecated and may go away.
             // *********
             var editor = CssColorPreview.getEditor();
-            var cm = editor._codeMirror;
-            var nLen = cm.lineCount();
-            var aColors = [];
+            if(editor) {
+                var cm = editor._codeMirror;
+                var nLen = cm.lineCount();
+                var aColors = [];
 
-            // match colors and push into an array
-            for(var i = 0; i < nLen; i++) {
-                var tempItem = cm.getLine(i).match(COLOR_REGEX);
-                if(tempItem) {
-                    tempItem = {
-                       line_number : i,
-                       color_value : tempItem
+                // match colors and push into an array
+                for(var i = 0; i < nLen; i++) {
+                    var tempItem = cm.getLine(i).match(COLOR_REGEX);
+                    // todo current support one color to show only
+                    if(tempItem){
+                        tempItem = tempItem[0].replace(';','');
+                        tempItem = {
+                           line_number : i,
+                           color_value : tempItem
+                        }
+                        aColors.push(tempItem);
                     }
-                    aColors.push(tempItem);
                 }
+                CssColorPreview.showGutters(editor, aColors);
             }
-            CssColorPreview.showGutters(editor, aColors);
         },
 
         onChanged : function () {
@@ -95,24 +102,26 @@ define(function (require, exports, module) {
 
         init: function() {
             var editor = CssColorPreview.getEditor();
-            var cm = editor._codeMirror;
-            if (_prefs.get("enabled")==="undefined") {
-                _prefs.definePreference("enabled", "boolean", _enabled);
-            } else {
-                _enabled = _prefs.get("enabled");
+            if(editor) {
+                var cm = editor._codeMirror;
+                if (_prefs.get("enabled")==="undefined") {
+                    _prefs.definePreference("enabled", "boolean", _enabled);
+                } else {
+                    _enabled = _prefs.get("enabled");
+                }
+                CommandManager.register("Enable Css Color Preview", COMMAND_NAME, CssColorPreview.toggleEnable);
+                // Then create a menu item bound to the command
+                Menus.getMenu(Menus.AppMenuBar.VIEW_MENU).addMenuItem(COMMAND_NAME);
+
+                // Set the starting state for the menu item.
+                CommandManager.get(COMMAND_NAME).setChecked(_enabled);
+
+                if(!_prefs.get("enabled")){return;}
+                CssColorPreview.showColorMarks();
+                // when activeEditorChange
+                $(DocumentManager).on("currentDocumentChange", CssColorPreview.onChanged);
+                cm.on("change", CssColorPreview.onChanged);
             }
-            CommandManager.register("Enable Css Color Preview", COMMAND_NAME, CssColorPreview.toggleEnable);
-            // Then create a menu item bound to the command
-            Menus.getMenu(Menus.AppMenuBar.VIEW_MENU).addMenuItem(COMMAND_NAME);
-
-            // Set the starting state for the menu item.
-            CommandManager.get(COMMAND_NAME).setChecked(_enabled);
-
-            if(!_prefs.get("enabled")){return;}
-            CssColorPreview.showColorMarks();
-            // when activeEditorChange
-            $(EditorManager).on("activeEditorChange", CssColorPreview.onChanged);
-            cm.on("change", CssColorPreview.onChanged);
         },
 
         initGutter: function(editor) {
@@ -127,29 +136,27 @@ define(function (require, exports, module) {
         },
 
         showGutters: function(editor, _results) {
-            CssColorPreview.initGutter(editor);
-            var cm = editor._codeMirror;
-            cm.clearGutter(gutterName); // clear color markers
-            cm.colorGutters = _.sortBy(_results, "line_number");
-            cm.clearGutter(gutterName);
-            cm.colorGutters.forEach(function (obj) {
-                var $marker = $("<i>")
-                                .addClass("ico-cssColorPreview")
-                                .html("&nbsp;").css('background-color',obj.color_value);
-                cm.setGutterMarker(obj.line_number, gutterName, $marker[0]);
-            });
+            if(editor){
+                CssColorPreview.initGutter(editor);
+                var cm = editor._codeMirror;
+                cm.clearGutter(gutterName); // clear color markers
+                cm.colorGutters = _.sortBy(_results, "line_number");
+                cm.clearGutter(gutterName);
+                cm.colorGutters.forEach(function (obj) {
+                    var $marker = $("<i>")
+                                    .addClass("ico-cssColorPreview")
+                                    .html("&nbsp;").css('background-color',obj.color_value);
+                    cm.setGutterMarker(obj.line_number, gutterName, $marker[0]);
+                });
+            }
         }
     }
 
     // init after appReady
     AppInit.appReady(function() {
+        var editor = EditorManager.getActiveEditor();
         // todo ?how to get the enabled editor instance
-        var _timer = setInterval(function () {
-            if(CssColorPreview.getEditor()!=null){
-                clearInterval(_timer);
-                CssColorPreview.init();
-            }
-        }, 100);
+        setTimeout(CssColorPreview.init,100);
     });
 
 });
